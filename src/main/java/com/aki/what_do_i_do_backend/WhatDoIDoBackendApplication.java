cc180d0767fdf8ce1a083c2426dc6f4d93d084d4
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-06-02 18:34:41 +0200

    initialising project

---
+++ src/main/java/com/aki/what_do_i_do_backend/WhatDoIDoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aki.what_do_i_do_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatDoIDoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatDoIDoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aki/what_do_i_do_backend/WhatDoIDoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aki.what_do_i_do_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatDoIDoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
