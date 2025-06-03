bf27dfd1c8ce3d684fad685ce8b076850ea913d2
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-06-03 11:18:49 +0200

    adding method in repository to only get 3 decisions from the db

--- src/main/java/com/aki/what_do_i_do_backend/repository/DecisionsRepository.java
+++ src/main/java/com/aki/what_do_i_do_backend/repository/DecisionsRepository.java
@@ -1,6 +1,7 @@
 package com.aki.what_do_i_do_backend.repository;
 
 import com.aki.what_do_i_do_backend.model.Decision;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +13,8 @@ public interface DecisionsRepository extends ListCrudRepository<Decision, UUID> 
     List<Decision> getAllByOwnerId(String ownerId);
 
     Decision findByOwnerIdAndId(String ownerId, UUID id);
+
+    @Query(value = "SELECT * FROM decisions ORDER BY RANDOM() LIMIT 3", nativeQuery = true)
+    List<Decision> findRandomDecisions();
+
 }
